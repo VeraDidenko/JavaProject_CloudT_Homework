a151defad04be40474bd6dfb0340f3ebf0b4dad2
VeraDidenko <31995385+VeraDidenko@users.noreply.github.com>
2018-02-12 00:55:46 +0200

    changed zip to source code

---
+++ src/special/Special.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package special;
+
+/**
+ *
+ * @author vera
+ */
+public class Special {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String project = "special";
+        if(project.equals("special"))
+        {
+           System.out.println("A Special Java Project :-)"); 
+        }
+        else
+        {
+            System.out.println("Add something new ;-)"); 
+        }
+        
+    }
+    
+}
